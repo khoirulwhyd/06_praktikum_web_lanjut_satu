bb5d6b723fbaa82ad12be5ea316dc17a4b2ae367
khoirulwhyd <gamingkenzo7@gmail.com>
2021-03-26 21:53:18 +0700

    PERTEMUAN 6

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class HomeController extends Controller
+
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        return view('home',['user' => $user]);
+    }
+}
